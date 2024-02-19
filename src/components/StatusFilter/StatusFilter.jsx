5fe4358ac47186fdb46428fa3093154d662a37b1
cefariron <adidaswww17@gmail.com>
2024-02-19 16:11:44 +0200

    complete test case

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -1,5 +1,6 @@
 import { useSelector, useDispatch } from "react-redux";
-import { setStatusFilter } from "../../redux/actions";
+// import { setStatusFilter } from "../../redux/actions";
+import { setStatusFilter } from "../../redux/filtersSlice";
 import { statusFilters } from "../../redux/constants";
 import { getStatusFilter } from "../../redux/selectors";
 import { Button } from '../Button/Button';
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,11 @@
-import { legacy_createStore as createStore } from 'redux';
-import { devToolsEnhancer } from "@redux-devtools/extension";
-import { rootReducer } from './reducers';
+import { configureStore } from '@reduxjs/toolkit';
+// import { tasksReducer, filtersReducer } from './reducers';
+import { tasksReducer } from "./tasksSlice";
+import { filtersReducer } from "./filtersSlice";
 
-const enhancer = devToolsEnhancer();
-export const store = createStore(rootReducer, enhancer);
+export const store = configureStore({
+    reducer: {
+        tasks: tasksReducer,
+        filters: filtersReducer,
+    },
+})
