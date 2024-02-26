7fe659b11d9a8d5c91337c59ad41f4d6bc599f2d
cefariron <adidaswww17@gmail.com>
2024-02-26 14:57:41 +0200

    Create PrivacyPolicyEN.tsx

---
+++ src/components/PrivacyPolicyEN.tsx
@@ -0,0 +1,134 @@
+// PrivacyPolicyEN.tsx
+import React from 'react';
+
+const PrivacyPolicyEN = () => {return (
+    <div className="pageWrap pb30 pt30">
+        <div className="containerBox">
+            <div className="sideHeader">
+                            <span className="pageTitle_icon">
+                                <img src="/img/icons/privacy_icon.svg" alt="Experiences" />
+                            </span>
+                <h1 className="pageTitle only"> Privacy</h1>
+            </div>
+        </div>
+        <div className="pt30">
+            <div className="containerBox">
+                <div className="legalContent">
+                    <div>
+                        <h3>Privacy Notice</h3>
+                        <p>With this privacy notice (the <strong>Notice</strong>), we inform you how we process your personal data in relation to our platform on www.livxp.com (the <strong>Platform</strong>).</p>
+                        <p>Your personal data means any information relating to you (<strong>Personal Data</strong>). Processing means any handling of your Personal Data such as collection, retention, storage, adaption, usage, transfer, disclosure etc. (<strong>Processing</strong>). If “<strong>LivXP</strong>”, “<strong>we</strong>” (or “<strong>us</strong>”) is used hereinafter, this primarily refers to LivXP AG, Rue du Valentin 1, 1004 Lausanne, Switzerland.</p>
+
+                        <h3>1. Who we are</h3>
+                        <p>We are responsible for the Processing of your Personal Data under this Notice and act as the controller in relation thereto. Should you have any questions in relation to this Notice or regarding the Processing of your Personal Data please contact us at:</p>
+                        <p>LivXP AG,<br />
+                            Rue du Valentin 1, 1004 Lausanne, Switzerland<br />
+                            Email: <a href="mailto:privacy@livxp.com">privacy@livxp.com</a></p>
+
+                        <h3 id="processData">2. Personal data we process and our sources</h3>
+                        <p>We may collect your Personal Data when you interact with us, for example when you communicate with our staff, purchase one of the products offered by other Platform users, or when signing up to receive information and/or brochures or other marketing.</p>
+                        <p>Personal data we process about you may include:</p>
+                        <ul>
+                            <li>Technical information when visiting and using the services of the Platform (e.g. your IP-address, browser, devicetype);</li>
+                            <li>Contact details (e.g. name, address, phone number, e-mail address, date of birth);</li>
+                            <li>Financial information (e.g. credit card number, amount of payment, transaction data);</li>
+                            <li>Information in relation to our services (e.g. your offers, your orders, your purchases, your shopping cards, your account registration, your role as a buyer or a vendor (including as a “Supporter” or an “Experience Provides” as defined in our Terms and Conditions);</li>
+                            <li>Additional information in relation to your visit of the Platform (e.g. product pages you visit, interaction with other users of the platform, information on how often you visit the Platform);</li>
+                            <li>Information in relation to materials and communications we send to you electronically (e.g. newsletters);</li>
+                            <li>Additional information provided to us in the course of providing our services to you (e.g. information to handle your requests, complaints, information on your communication with us via phone, chat, e-mail).</li>
+                        </ul>
+
+                        <h3>3. How and what purpose for we use your Personal Data</h3>
+                        <p>We use your Personal Data:</p>
+                        <ol>
+                            <li>To provide and improve our services to you and our customers, including Personal Data of others provided to us or collected by us on behalf of our clients, for example Processing identification and background information as part of our Platform, finance and administration purposes as well as customer relationship management (e.g. customer management, billing purposes, newsletter services);</li>
+                            <li>To provide and improve our Platform, including monitoring and evaluating its use;</li>
+                            <li>For customer care and to promote our services, including by sending alerts, updates and marketing communication;</li>
+                            <li>To comply with our legal, regulatory and risk management obligations, including establishing, exercising or defending legal claims.</li>
+                        </ol>
+
+                        <h3>4. Automated decision-making</h3>
+                        <p>By “automated decision-making” is meant any Processing which results in a decision that is based solely on automated processing including profiling (i.e. without any human involvement, for example based on automated algorithm technics), which produces legal effects or similar significant effects on the data subjects.</p>
+                        <p>We do not use any Processing that is based on such decision-making processes.</p>
+
+                        <h3>5. On what basis we use your personal information</h3>
+                        <p>We process your Personal Data on the following grounds:</p>
+                        <ul>
+                            <li>to perform a contract;</li>
+                            <li>for the establishment, exercise or defence of legal claims or proceedings;</li>
+                            <li>to comply with legal and regulatory obligations;</li>
+                            <li>for legitimate interests.</li>
+                        </ul>
+                        <p>Please see <a href="#processData">“How we process your Personal Data”</a> above for additional details.</p>
+
+                        <h3>6. Who we share your Personal Data with</h3>
+                        <p>We may share your Personal Data with trusted third parties who process your Personal Data on our behalf including such as:</p>
+                        <ul>
+                            <li>service provider (e.g. IT-/Hosting-Provider IT service providers; payment service providers);</li>
+                            <li>other parties (e.g. lawyers, debt collection and credit agencies, consultants, auditors).</li>
+                            <li>[others]</li>
+                        </ul>
+                        <p>In addition, we may disclose Personal Data if we are required or permitted to do so by law or legal process, for example due to a court order or a request from a law enforcement agency, when we believe disclosure is necessary or appropriate for our legitimate interest, for example in preventing harm or financial loss, in connection with an investigation of suspected or actual fraudulent or other illegal activity, and in the event we sell or transfer all or a portion of our business or assets (including in the event of a reorganization, dissolution, or liquidation).</p>
+                        <p>We do not disclose your Personal Data to other parties that might use your Personal Data for their own purposes, in particular we do not share your Personal Data with others for third party marketing.</p>
+
+                        <h3>7. Countries to which we transfer your Personal Data</h3>
+                        <p>We may transfer your Personal Data to recipients abroad (they may even be located outside the European Union), including countries that do not provide the same level of protection as Swiss and/or EU/EEA legislation. Before we do so we will implement appropriate safeguards to protect your Personal Data, for example by requiring the recipient to agree to data Processing agreements. If you wish to receive a copy of the respective safeguards, please contact us at the contact details in section 1 <a href="#">“Who we are”</a>.</p>
+                        <p>We may also transfer Personal Data with your explicit consent and in certain other situations as permitted by applicable law.</p>
+
+                        <h3>8. Cookies</h3>
+                        <p>We use cookies on our site. Cookies are small files that your browser automatically creates and that are stored on your device (laptop, tablet, smartphone, etc.) when you visit our Platform.</p>
+                        <p>The cookie is used to store information that is related to the specific device used. However, this does not mean that we are immediately aware of your identity.</p>
+                        <p>The use of cookies serves to make use of our Platform more pleasant for you. For example, we use so-called session cookies to recognize that you have already visited individual pages on our website. These are automatically deleted after leaving our pages.</p>
+                        <p>In addition, to improve usability, we also use temporary cookies that are stored on your device for a specified period of time. If you visit our Platform again to take advantage of our services, it will automatically recognize that you have already been with us and what inputs and settings you have made, so you do not have to re-enter them.</p>
+                        <p>We also use cookies to statistically record the use of our Platform and to evaluate it for the purpose of optimizing our content and to show you content that meets your personal interests.</p>
+                        <p>Most browsers accept cookies automatically. However, you can configure your browser so that no cookies are stored on your computer, or so that a prompt appears before a new cookie is created. However, disabling cookies completely may mean that you cannot use all features of our website.</p>
+                        <p>Whenever we use cookies that are not technically required for the use and maintenance of our Platform, we will request your consent.</p>
+
+                        <h3>9. Tracking, Targeting and third-party social plug-ins</h3>
+                        <p>We use various tracking and targeting tools incl. analytic services. Please note that we are not responsible for the processing by these third parties. If you wish to learn more about on how these parties process your Personal Data please see their own privacy notices.</p>
+                        <p>Our websites use Google Analytics, a web analytics service provided by Google LLC (1600 Amphitheater Parkway, Mountain View, CA 94043, hereinafter "Google"). In this context, pseudonymized usage profiles are created and cookies (see section 8) are used. Information is generated by the cookie about your use of the website such as:</p>
+                        <ul>
+                            <li>browser type / version,</li>
+                            <li>used operating system,</li>
+                            <li>Referrer URL (the previously visited page),</li>
+                            <li>host name of the accessing terminal (IP address),</li>
+                            <li>time of server request,</li>
+                        </ul>
+                        <p>and are transmitted to a Google server in the US and stored there. The information is used to evaluate the use of the website, to compile reports on website activity, and to provide other services related to website activity and internet usage for the purposes of market research and tailor our website design. This information may also be transferred to third parties if required by law or if third parties process this data on our behalf. Under no circumstances will your IP address be merged with any other data provided by Google. The IP addresses are anonymized, so that such a merger is not possible (IP masking).</p>
+                        <p>You can prevent the installation of cookies by setting your browser software accordingly; however, we must point out that in this case not all features of our website may be fully functional.</p>
+                        <p>In addition, you may prevent the collection of data generated by the cookie related to your use of the Platform and the Processing of this data by Google by downloading and installing a browser add-on.</p>
+                        <p>As an alternative to the browser add-on, especially for browsers on mobile devices, you can prevent the collection of data by Google Analytics by clicking on this link. An opt-out cookie will be set that will prevent the future collection of your data when you visit this website. The opt-out cookie is only valid on that browser and only for our website and is stored on your device. If you delete cookies in that browser, you must set the opt-out cookie again.</p>
+
+                        <h3>10. How we retain your Personal Data</h3>
+                        <p>We retain your Personal Data for as long as it is necessary for the purposes for which the data is collected, and as long as we have a legitimate interest in keeping Personal Data (for example to enforce or defend claims or for archiving purposes and IT security). We also retain your Personal Data as long as it is subject to a legal retention obligation.</p>
+
+                        <h3>11. How we protect your personal information</h3>
+                        <p>We use various technical and organisational measures to help protect your Personal Data from unauthorised access, use, disclosure, alteration or destruction and to ensure an adequate data protection and data security level when Processing your Personal Data.</p>
+
+                        <h3>12. Your rights</h3>
+                        <p>You have the following rights:</p>
+                        <ul>
+                            <li>You are entitled to ask for details of the information we hold about you and how we process it and to receive a copy of your Personal Data.</li>
+                            <li>You may also have your data rectified or deleted, restrict our Processing of that information, and object to the Processing of your Personal Data.</li>
+                            <li>You may also choose to withdraw your consent. Please note that even after you have chosen to withdraw your consent we may be able to continue to process your personal information to the extent required or permitted by law.</li>
+                            <li>You may also lodge a complaint in relation to our Processing of your Personal Data with a local supervisory authority, in Switzerland with the Federal Data Protection and Information Commissioner.</li>
+                        </ul>
+                        <p>We must ensure that your Personal Data is accurate and up to date. Therefore, to exercise your rights or in the event of any changes to your Personal Data you shared with us, please contact us via the channels as indicated above in section 1 <a href="#">“Who we are”</a>.</p>
+
+                        <h3>13. Changes to this Notice</h3>
+                        <p>This Notice may be adjusted over time, especially if we change our data Processing or if new legislation becomes applicable. We actively inform persons whose contact details are registered with us of such changes in the event the changes are significant, if this is possible without disproportionate effort. In general, however, the data protection declaration in its current version at the beginning of the respective Processing applies to data Processing.</p>
+
+                    </div>
+
+                    <div className="versionBox">
+                        {' '}
+                        <small>Last update: February 2024</small>{' '}
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+)};
+
+export default PrivacyPolicyEN;
